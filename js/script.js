066833750c951ef4054ce8a9f6efa108c14d2a05
Marco <marco.cusenza.me@gmail.com>
2021-11-29 17:57:35 +0100

    milestone 1 + milestone 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 const app = new Vue({
   el: '#root',
   data: {
-
+    todo: [
+      {
+        text: 'Fare la spesa',
+        done: true,
+      },
+      {
+        text: 'Portare fuori il cane',
+        done: false,
+      },
+      {
+        text: 'Cucinare il pranzo',
+        done: false,
+      }
+    ],
+    inputText: '',
   },
   methods: {
-
+    removeEl: function (index) {
+      this.todo.splice(index, 1);
+      console.log(this.todo);
+    },
+    addEl: function () {
+      if (this.inputText != '') {
+        const el = {
+          text: this.inputText,
+          done: false,
+        }
+        this.todo.push(el);
+      } else {
+        alert("Inserisci del testo prima di aggiungere");
+      }
+      this.inputText = '';
+    }
   },
 });
